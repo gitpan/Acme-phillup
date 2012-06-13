11868fe1e750ecc2413e60f4dc4243ea697fb886
Phillip W. Upton <phillup@cpan.org>
2012-06-13 20:18:38 +0000

    Import of PHILLUP/Acme-phillup-0.02 from CPAN.
    gitpan-cpan-distribution: Acme-phillup
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         PHILLUP/Acme-phillup-0.02.tar.gz
    gitpan-cpan-author:       PHILLUP
    gitpan-cpan-maturity:     released

--- lib/Acme/phillup.pm
+++ lib/Acme/phillup.pm
@@ -1,6 +1,6 @@
 package Acme::phillup;
 
-use 5.006;
+use 5.006
 use strict;
 use warnings;
 
@@ -10,23 +10,16 @@ Acme::phillup - The great new Acme::phillup!
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Acme::phillup;
-
-    my $foo = Acme::phillup->new();
-    ...
+This is just a quickie module being created during a workshop at YAPC::NA 2012 by brian d foy
 
 =head1 EXPORT
 
